7ebcb2dc86329535e9b685b75def008e8f152663
EvanO3 <Evan.oni@hotmail.com>
2024-01-04 23:02:14 -0500

    Added information about myself

---
+++ Portfolio/src/Components/About.jsx
@@ -0,0 +1,11 @@
+import "../Css/App.css"
+export default function About(){
+    return(
+        <>
+        <section>
+        <p>I am a Informationn Technology student with a strong passion for coding. <br/>My ultimate aspiration is to become a proficient full-stack developer <br/>crafting exceptional web applications that leave a lasting impact</p>
+
+        </section>
+        </>
+    )
+}
