17dd48102ad9777f8f59e4e677b33c21a36eb91b
EvanO3 <Evan.oni@hotmail.com>
2024-01-04 23:29:05 -0500

    changed background

--- Portfolio/src/main.jsx
+++ Portfolio/src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Home from './Components/Home.jsx'
+import About from './Components/About.jsx'
 import"./Css/index.css"
 import"./Css/reset.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Home />
+    <About/>
   </React.StrictMode>,
 )
