5aee85cdfba58e7f012024a48565b01acb9afb54
EvanO3 <Evan.oni@hotmail.com>
2024-01-07 18:04:15 -0500

    added information about myself and photo to complete about me page

--- Portfolio/src/Components/About.jsx
+++ Portfolio/src/Components/About.jsx
@@ -1,11 +1,52 @@
 import "../Css/App.css"
-export default function About(){
-    return(
-        <>
-        <section>
-        <p>I am a Informationn Technology student with a strong passion for coding. <br/>My ultimate aspiration is to become a proficient full-stack developer <br/>crafting exceptional web applications that leave a lasting impact</p>
+import {forwardRef} from 'react'
+import photo from '../img/Photo.jpg'
+import {useEffect} from "react"
+import AOS from "aos"
+import "aos/dist/aos.css"
 
-        </section>
-        </>
-    )
-}
+const About =forwardRef((props, ref)=>{
+    useEffect(()=>{
+        AOS.init({
+          disable:"phone",
+          duration:700,
+          easing:"ease-out-cubic"
+        })
+          },[])
+
+return(
+    <section ref={ref}>
+
+    <div className="About-me" data-aos ="fade-right">
+    <div className="about-left">
+        <h2>About Me</h2>
+        <p>I am an ambitious Information Technology student<br/> fueled by a strong passion for coding. 
+        my goal is to<br/> excel as a dynamic full-stack developer, <span className="tackle">tackling challanges</span><br/>
+        head-on  innovating to create impactful web applications<br/> that resonate long after deployment
+        </p>
+
+        <p>My academic background equps me with a solid foundation in programming <br/>
+           languages and software development methodologies. However, it is my hands-on <br/>
+          experince and commitment to continuous learning that sets me apart.
+          through <br/> internships and personal projects, I have honed my coding abilities and gained <br/> practical insights 
+          into building robust and scalable applications
+        
+        </p>
+      </div>
+      <div className="About-rightCon">
+        <div className="About-right">
+            <img  className="imgPhoto" src={photo} style ={{width:"200px"}} alt="" />
+        </div>
+      </div> 
+    </div>
+   
+ 
+    
+</section>
+
+)
+
+})
+About.displayName = 'About';
+
+export  default About
