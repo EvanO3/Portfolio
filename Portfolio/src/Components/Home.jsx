f872a0b1cf470efbbbfc8ca628b3d69e3d4ea9f4
EvanO3 <Evan.oni@hotmail.com>
2024-01-06 20:44:21 -0500

    added memoji img and fixed layout

--- Portfolio/src/Components/Home.jsx
+++ Portfolio/src/Components/Home.jsx
@@ -1,15 +1,29 @@
 
 import"../Css/App.css"
 import"../Css/reset.css"
-import { LuMoonStar } from "react-icons/lu";
-import { FaArrowDown } from "react-icons/fa";
-
+//imported logos from library
 import { BsLinkedin } from "react-icons/bs";
 import { FaGithub } from "react-icons/fa";
-
+import { LuMoonStar } from "react-icons/lu";
+import { FaArrowDown } from "react-icons/fa";
+import { useEffect } from "react";
+//imported animation from libaray
+import AOS from "aos"
+import "aos/dist/aos.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Memoji from "../img/Memoji.jpg"
 
 export default function Home(){
+
+  useEffect(()=>{
+AOS.init({
+  disable:"phone",
+  duration:700,
+  easing:"ease-ouut-cubic"
+})
+  },[])
+
+  
 
   return(
     <>
@@ -25,39 +39,47 @@ export default function Home(){
     <a className="list-item"href="#">About</a>
     <a className="list-item"href="#">Skills</a>
     <a className="list-item"href="#">Projects</a>
+      <a className="list-item"href="#">Contact</a>
       <a className ="list-item" href="">
         <LuMoonStar
         style={{fontSize: "25px"}}
         />
         </a>
-      <a className="list-item"href="#">Contact</a>
     </li>
   </div>
 </nav>
 
 {/**Main body */}
 <section>
-<div className="body-left">
+
+<div data-aos ="fade-right"className="body-left">
+  
   <div className="left-text">
     <span style={{fontSize: "20px"}}>Hi There <p className="Wave">👋🏾</p> I'm</span>
-    <h1 style={{fontSize: "50px"}}>Evan Oni</h1>
-    <h1 style={{fontSize: "50px"}}>Information Technology <br/>Student</h1>
-    
-  </div>
-  <div className="animateArrow">
+    <h1 style={{fontSize: "40px"}}>Evan Oni</h1>
+    <h1 style={{fontSize: "40px"}}>Information Technology <br/>Student</h1>
+    <div className="animateArrow">
   <FaArrowDown
   style ={{fontSize:"30px"}}
-  
   />
   </div>
+  </div>
+<div className="right-container">
+<div className="right-text">
+<img className="imgMemoji" style ={{width: "300px"}}src={Memoji} alt="" />
+</div>
+</div>
+
+
+
+
+
+  
 </div>
 </section>
 
 
-<div className="body-right">
 
-  
-</div>
 
 
  
