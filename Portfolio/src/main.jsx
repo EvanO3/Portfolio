17831997bf32c9be977a8531d51a235f5410a7bc
EvanO3 <Evan.oni@hotmail.com>
2024-01-08 17:17:46 -0500

    rendered home component

--- Portfolio/src/main.jsx
+++ Portfolio/src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Home from './Components/Home.jsx'
-import About from './Components/About.jsx'
-import Skills from "./Components/Skills.jsx"
+
+
 import"./Css/index.css"
 import"./Css/reset.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Home />
-    <Skills/>
+    
     
   </React.StrictMode>,
 )
